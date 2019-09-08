83887219b7e8d19e63a773177535e363173f274e
96521227 <amir.reazei1999@yahoo.com>
2019-09-08 18:38:07 +0430

    Add very basic Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Navbar.scss";
+const NavbarComponent = () => {
+    return (
+        <div className="NavbarTop">
+            <button className={"bar-button"} type="button" >
+                منو
+            </button>
+        </div>
+    );
+};
+
+export default NavbarComponent;
