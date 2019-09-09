a280ff475541cc62872d11791f85e1eb6b12b70d
96521227 <amir.reazei1999@yahoo.com>
2019-09-09 14:27:48 +0430

    Change Navbar to fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import "./Navbar.scss";
+import LOGO from "../../assets/logo.png";
+// import "../../assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css";
+// import "./assets/js/plugins/nucleo/css/nucleo.css";
+import {IoMdMenu} from "react-icons/io";
 const NavbarComponent = () => {
     return (
         <div className="NavbarTop">
-            <button className={"bar-button"} type="button" >
-                منو
-            </button>
+            <img width={"3.5%"} className="ml-2 py-2" alt={"keiketoBesaz"} src = {LOGO}/>
+            <div className="float-right h-auto" >
+                <IoMdMenu className="MenuBar"/>
+            </div>
         </div>
     );
 };
